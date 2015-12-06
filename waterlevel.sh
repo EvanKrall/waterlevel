a71278bfb9bb6389f45a83c55748b52030ed73c2
Evan Krall <meatmanek@gmail.com>
2015-12-06 22:06:47 +0000

    switch to bash implementation because the python one was acting strangely. Use a different technique for testing conductivity: use the bottom pin as an output and test for connectivity between it and each pin by toggling it.

---
+++ waterlevel.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#!/bin/bash
+
+PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
+SLOTS=/sys/devices/platform/bone_capemgr/slots
+FIRMWARE_DIR=/lib/firmware
+GPIO=/sys/class/gpio
+
+# Uninstall previous copies of the overlay
+for layer in $(grep ',waterlevel$' "$SLOTS" | cut -f1 -d:) ; do
+	echo -$layer > "$SLOTS"
+done
+
+cp waterlevel-00A0.dtbo "$FIRMWARE_DIR"
+echo waterlevel > "$SLOTS"
+
+GPIOS=(66 67 69 68 45 44 23 26 47 46)
+
+BOTTOM_PIN=${GPIOS[0]}
+
+for pin in "${GPIOS[@]}"; do
+	echo "$pin" > $GPIO/export
+	if [[ $pin == $BOTTOM_PIN ]]; then
+		echo "out" > $GPIO/gpio$pin/direction
+		echo "0" > $GPIO/gpio$pin/value
+	else
+		echo "in" > $GPIO/gpio$pin/direction
+	fi
+	cat $GPIO/gpio$pin/direction
+done
+
+depth=0
+
+for ((i=1; i<${#GPIOS[@]}; i++)); do
+	pin="${GPIOS[i]}"
+	echo 0 > $GPIO/gpio$BOTTOM_PIN/value
+	value_1=$(< $GPIO/gpio$pin/value)
+	echo 1 > $GPIO/gpio$BOTTOM_PIN/value
+	value_2=$(< $GPIO/gpio$pin/value)
+	echo 0 > $GPIO/gpio$BOTTOM_PIN/value
+
+	if [[ $value_1 == 0 && $value_2 == 1 ]]; then
+		depth=$i
+	fi
+done
+
+echo $depth
